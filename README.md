bfe67570af4a854ba56687e0a8105aa5ba62bc0a
Chris Hunt <chris.hunt@hudl.com>
2023-11-28 08:00:29 +0000

    feat: initial add of files

---
+++ README.md
@@ -0,0 +1,6 @@
+# TDD Demo
+
+## Run Pytest Watcher
+```bash
+poetry run ptw .
+```
---
+++ main_test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+# Format Money
+# * Takes a value and an optional currency and outputs money to 2dp
+# * Currency is accepted as a three letter code
+# * Currency maps to a symbol
+# * If currency doesn't exist, exception is thrown
+# * If value is less than zero, exception is thrown 
+
+class TestFormatMoney(TestCase):
+    pass
