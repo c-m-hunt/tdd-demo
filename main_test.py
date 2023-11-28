fa6191d005eb51708433fab04099c137cd8649fe
Chris Hunt <chris.hunt@hudl.com>
2023-11-28 08:01:45 +0000

    feat: add new condition for method

--- main_test.py
+++ main_test.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 # Format Money
 # * Takes a value and an optional currency and outputs money to 2dp
+# * Default currency is GBP
 # * Currency is accepted as a three letter code
 # * Currency maps to a symbol
 # * If currency doesn't exist, exception is thrown
